10551054e398e61a52c1ada1894d9ede0227d2d8
Lucas Mendoza <lukmedellin@gmail.com>
2022-07-19 09:32:58 -0500

    tipos de datos primitivos

--- clase2/scripts/index.js
+++ clase2/scripts/index.js
@@ -1,5 +1,8 @@
-console.log(age);
+let age=5;
+let firstname="yeisson"
+let lastname="Arcila Vasquez"
+let isStudent=false
+let city
+let country=null
 
-console.log(nombre)
-
-let nombre="carlos"
+console.log(typeof country)
