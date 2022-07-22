898dad6a419166a14f6429b984b911ae9792d9f9
yeissonArcila <arcilavasquez6@gmail.com>
2022-07-22 09:56:34 -0500

    funciones nativas para numeros

---
+++ clase 4/assets/funcionesNumeros.js
@@ -0,0 +1,25 @@
+console.log(Math.E)//
+console.log(Math.PI)
+
+console.log(Math.pow(2,2))
+console.log(2**3)
+
+let numero1=30;
+let numero2=82;
+
+console.log(Math.min(numero1,numero2))
+console.log(Math.max(numero1,numero2))
+
+const numeroPI=3.1416
+
+console.log(Math.round(numeroPI))
+console.log(Math.ceil(numeroPI))
+
+console.log(Math.random()*(numero2-numero1)+numero1)
+console.log(Math.trunc (Math.random()*(numero2-numero1)+numero1))
+
+const numeros =[0, 20, 10, 5, 30]
+
+console.log(typeof numeros)
+console.log(`el numero menor es: ${Math.min(numeros)}`);
+console.log(`el numero mayor es: ${Math.max(numeros)}`)
