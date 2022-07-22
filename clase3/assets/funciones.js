686d169c5881e8c71d5764756cf6a96a77271ddd
yeissonArcila <arcilavasquez6@gmail.com>
2022-07-22 09:28:32 -0500

    funciones nativas para caracteres

---
+++ clase3/assets/funciones.js
@@ -0,0 +1,48 @@
+//funcion normal
+function saludar(){
+    console.log("hola mundo")
+}
+
+// saludar();
+// saludar();
+// saludar();
+
+//funcion tipo flecha
+const saludo=(nombre)=>{
+console.log(`hola ${nombre}`)
+}
+saludo("juan");
+saludo("isabella")
+saludo("santiago")
+
+
+function sumar(){
+    let suma=2+3;
+    console.log(suma)
+}
+sumar()
+
+const suma=(num1,num2)=>{
+    let resultado=num1+num2;
+    console.log(resultado)
+
+}
+suma (5,10)
+suma(10,90)
+
+const multiplicar=(num1,num2)=>{
+    let resultado=num1*num2
+    console.log(resultado)
+}
+multiplicar(4,10)
+multiplicar(50,10)
+
+let 
+
+
+// function edad(){
+//     let edad=prompt("ingrese su edad")
+//     console.log(edad)
+// }
+// edad()
+
---
+++ clase3/assets/objetos.js
@@ -0,0 +1,29 @@
+//crear objeto vacio´
+let objeto={}
+console.log(objeto)
+//creando objeto carro
+let carro={
+    color: "rojo",
+    velocidadMaxima: "100km/h",
+    modelo:2020,
+    placa:"ABC123",
+    //propetiarios:{"stiven", "yinier"},
+    lugarCreacion:{
+        ciudad:"Medellin",
+        pais:"Colombia"
+    },
+    tieneSoat:true
+
+
+}
+console.log(carro)
+
+const computador={
+    ram:"16 gb de ramm",
+    modelo:2020,
+    graficos:"intel core",
+    discoDuro:"500 gb",
+
+
+}
+console.log(computador)
