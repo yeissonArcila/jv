b3e321127e035410ff05e145a186dd14a2e3911f
yeisson <arcilavasquez6@gmail.com>
2022-07-25 10:06:33 -0500

    clase 5

--- clase1/scripts/app.js
+++
@@ -1,4 +0,0 @@
-let nombre=prompt("ingrese su nombre");
-let apellido=prompt("ingrese su apellido");
-
-console.log(nombre+apellido);
--- clase2/scripts/index.js
+++
@@ -1,8 +0,0 @@
-let age=5;
-let firstname="yeisson"
-let lastname="Arcila Vasquez"
-let isStudent=false
-let city
-let country=null
-
-console.log(typeof country)
